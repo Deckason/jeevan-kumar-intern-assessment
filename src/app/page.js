68997a43f3afa4852798b4ea61644fed74868fd1
Deckason <chibuikemchidera@gmail.com>
2024-04-27 08:53:38 +0100

    Created the navBar

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,14 @@
+
 import React from 'react'
+
+import Nav from './components/nav/Nav';
 
 const page = () => {
   return (
-    <div>page</div>
+    <div className='container'>
+      <Nav />
+
+    </div>
   )
 }
 
